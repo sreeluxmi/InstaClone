82774df077a609412083315def9c3d7f3d739af6
sreeluxmi <sriluxmisali@gmail.com>
2023-09-25 08:04:45 +0530

    post

--- src/static/post/js/postImage.js
+++ src/static/post/js/postImage.js
@@ -1,38 +1,44 @@
-document.getElementById("imagePostingForm").addEventListener("submit", function (event) {
-    event.preventDefault()
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("imagePostingForm").addEventListener("submit", function (event) {
+        event.preventDefault()
+        var access_token = localStorage.getItem('access_token')
 
-    var access_token = localStorage.getItem('access_token')
-
-    if (access_token){
-        const imageInput = document.getElementById("images");
-        console.log(imageInput)
-        const captionInput = document.getElementById("caption");
-
-        const formData = new FormData();
-        const images = imageInput.files;
-
-        for (let i = 0; i < images.length; i++) {
-            formData.append("uploaded_images", images[i]);
+        if (access_token){
+            const imageInput = document.getElementById("images");
+            console.log(imageInput)
+            const captionInput = document.getElementById("caption");
+    
+            const formData = new FormData();
+            const images = imageInput.files;
+    
+            if (images.length > 0) {
+                for (let i = 0; i < images.length; i++) {
+                    formData.append("uploaded_images", images[i]);
+                }
+            } else {
+                formData.append("uploaded_images", imageInput.files[0]);
+            }
+            
+    
+            formData.append("caption", captionInput.value);
+    
+            fetch("/users/api/posts/", {
+                method: "POST",
+                headers: {
+                    "Authorization" : "Bearer " + access_token
+                },
+                body: formData,
+            })
+            .then(function (response){
+                if (response.ok){
+                    console.log("success")
+                }else{
+                    console.log("Error")
+                }
+            })
+        }else{
+            window.location.href = '/users/home/';
         }
 
-        formData.append("caption", captionInput.value);
-
-        fetch("/users/api/posts/", {
-            method: "POST",
-            headers: {
-                "Authorization" : "Bearer " + access_token
-            },
-            body: formData,
-        })
-        .then(function (response){
-            if (response.ok){
-                console.log("success")
-            }else{
-                console.log("Error")
-            }
-        })
-    }else{
-        window.location.href = '/users/home/';
-    }
+    });
 });
-         
